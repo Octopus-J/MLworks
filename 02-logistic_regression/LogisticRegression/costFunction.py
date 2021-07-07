2f25642730b37523ea173fbc89d965d7b9c12ee3
章鱼大侠 <82190911+Octopus-J@users.noreply.github.com>
2021-07-07 15:55:44 +0800

    Update costFunction.py

--- 02-logistic_regression/LogisticRegression/costFunction.py
+++ 02-logistic_regression/LogisticRegression/costFunction.py
@@ -5,13 +5,13 @@ def costFunction(theta,x,y,flag=0,lamda=0):    # flag is used to control process
     hyp=sig.sigmoidFunction(theta,x)
     epsilon=1.0e-200                           # epsilon is a very small constan, the reason to use it is to avoid the np.log(0),this problem troubled me a lot
     if flag==0:
-        cost1=-(y.T)*(np.log(hyp+epsilon))
+        cost1=-y*(np.log(hyp+epsilon))
         cost2=-(1-y)*(np.log(1-hyp+epsilon))
         cost=np.sum(cost1+cost2)/len(y)
     else:
         _theta=theta[1:]                       
         _theta=np.insert(_theta,0,0)           # in python, if a parameter is variable,then in the transfer behavior,itself could be changed
-        cost1=-(y.T)*(np.log(hyp+epsilon))
+        cost1=-y*(np.log(hyp+epsilon))
         cost2=-(1-y)*(np.log(1-hyp+epsilon))
         cost=np.sum(cost1+cost2)/(2*len(y))+(lamda/(2*len(y)))*np.sum(np.power(_theta,2))
 
